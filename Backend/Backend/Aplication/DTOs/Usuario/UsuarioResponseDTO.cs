4d66799e261196e80b61bea9d30a81f706972ea8
Stefany Cruz <stefanycruz1726@gmail.com>
2025-03-17 08:48:13 -0600

    feature:se agrego dtos de response y request de usuario

---
+++ Backend/Backend/Aplication/DTOs/Usuario/UsuarioResponseDTO.cs
@@ -0,0 +1,10 @@
+namespace Aplication.DTOs.Usuario
+{
+    public class UsuarioResponseDTO
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+    }
+}
+
--- Backend/Backend/Aplication/obj/Release/net8.0/Aplication.AssemblyInfo.cs
+++ Backend/Backend/Aplication/obj/Release/net8.0/Aplication.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplication")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5540dcb8603f73212e3e81cbc5c3193570f33241")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplication")]
