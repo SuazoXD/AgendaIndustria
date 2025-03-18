a6563a7fbe9b34da49e1530f870236a48a2b190e
SuazoXD <suazocarlos50@gmail.com>
2025-03-18 00:05:04 -0600

    agregando dominio nuevamente

---
+++ Backend/Backend/Domain/Common/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Common
+{
+    // Clase base para todas las entidades, definiendo una propiedad Id.
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Backend/Backend/Domain/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Usuario : Entity
+    {
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string?   Contrasenia { get; set; }
+    }
+}
