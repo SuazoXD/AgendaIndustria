7cab9a56ac10e6abdb5f53f233730cca0dda4dd7
SuazoXD <suazocarlos50@gmail.com>
2025-03-17 22:43:40 -0600

    Creando entidades en Dominio

---
+++ Backend/Backend/Domain/AggregateRoots/Package/Package.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Domain.Common;
+
+namespace Domain.AggregateRoots.Package
+{
+    public class Package : AggregateRoot
+    {
+        public string? Name { get; set; }
+        public ICollection<PackageDetail> Details { get; set; } = [];
+    }
+}
---
+++ Backend/Backend/Domain/Entities/Tipo.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    // Hereda de EntityCatalog para aprovechar propiedades comunes (por ejemplo, Nombre)
+    public class Tipo : EntityCatalog
+    {
+        public string? Descripcion { get; set; }
+    }
+}
