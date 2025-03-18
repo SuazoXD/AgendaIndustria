cce2962d9779b9130b927a72d224a0b5ac3e3837
SuazoXD <suazocarlos50@gmail.com>
2025-03-17 23:50:22 -0600

    Revert "Merge pull request #10 from SuazoXD/feature/CarlosSuazo/202503/crear-entidades-base"
    This reverts commit 37bcaecdf399cad54e6ea8b54851688e58d09693, reversing
    changes made to 90dbc38a8dca270bc2d1177f85279d26ad6b56f9.

--- Backend/Backend/Domain/Common/AggregateRoot.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Common
-{
-    // Clase base para las entidades que actúan como Aggregate Roots.
-    public abstract class AggregateRoot : Entity
-    {
-        // Aquí podrías agregar lógica común a todos los aggregate roots.
-    }
-}
